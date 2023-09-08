16120d2bc7f28b59a68ae8bd55572e59cd1457f5
MartinPerezGiordan <martin.perezgiordan@gmail.com>
2023-09-08 10:52:41 -0300

    Add class Invitacion, Miembro y enum EstadoSolicitud

---
+++ Dominio/Miembro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorio1_P2
+{
+    internal class Miembro
+    {
+        #region Atributos
+        private static int _ultimoId;
+        private int _id;
+        private string _email;
+
+        #endregion
+
+
+
+        #region Metodos
+        #endregion
+
+
+    }
+}
