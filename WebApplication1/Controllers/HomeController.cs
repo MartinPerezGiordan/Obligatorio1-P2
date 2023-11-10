e424decd4dae5cff6c712bda66e8cc3b6a4395f9
MartinPerezGiordan <martin.perezgiordan@gmail.com>
2023-11-10 10:30:37 -0300

    Implementacion del MVC al dominio del Obligatorio 1

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dominio;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.nombre = "Pedro Paez";
+            return View();
+        }
+    }
+}
