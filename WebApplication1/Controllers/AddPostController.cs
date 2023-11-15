e0f515142306c393ec12d52a73fcb196aabbe15d
MartinPerezGiordan <martin.perezgiordan@gmail.com>
2023-11-15 15:43:40 -0300

    Feat addPost implementado con error -> publico y privado no funciona bien

---
+++ WebApplication1/Controllers/AddPostController.cs
@@ -0,0 +1,32 @@
+﻿using Dominio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AddPostController : Controller
+    {
+
+        public IActionResult AddPost(string mensaje)
+        {
+            ViewBag.MensajeExito = mensaje;
+            return View("AddPost");
+        }
+
+        [HttpPost]
+        public IActionResult AddPost(string Titulo, string Image, string Texto, bool Publico)
+        {
+            Miembro miembroLogeado = Sistema.Instancia.GetMiembroByEmail(HttpContext.Session.GetString("usuario"));
+            try
+            {
+                Sistema.Instancia.AgregarPostMiembro(miembroLogeado.Id, Titulo, Texto, Image, Publico);
+                return RedirectToAction("Index", "Home", new { mensajeExito = "Post creado con exito" });
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Mensaje = ex.Message;
+                return View();
+            }
+
+        }
+    }
+}
